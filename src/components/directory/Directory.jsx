a056bf16ba74317b558c4f5fe9ad36fd95698d52
Simon Catalin <simoncatalin@Simons-MacBook-Air.local>
2025-02-24 17:31:20 +0200

    first commit - directory page with directory component and category item component

---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import CategoryItem from "../category-item/CategoryItem";
+import "./directory.styles.scss";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
