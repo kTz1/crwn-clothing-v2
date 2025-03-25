8553000d354d301b7157b9fbd1755a9a83c5f063
kTz1 <catalin_simon90@yahoo.com>
2025-03-25 19:03:23 +0200

    feat: integrate Stripe for payment processing

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { Provider } from "react-redux";
-//import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 import App from "./App.jsx";
 import { store } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 import "./index.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      {/*<PersistGate loading={null} persistor={persistor}>*/}
       <BrowserRouter>
-        <App />
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
       </BrowserRouter>
-      {/*</PersistGate>*/}
     </Provider>
   </StrictMode>
 );
---
+++ src/utils/stripe/stripe.utils.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+import { loadStripe } from "@stripe/stripe-js";
+
+export const stripePromise = loadStripe(
+  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+);
