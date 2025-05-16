161a2fb99f0c6135548aac7ff999cdb21d8e4ed1
kTz1 <catalin_simon90@yahoo.com>
2025-05-16 11:32:06 +0300

    refactor: implement lazy loading for improved performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router";
-import Home from "./routes/home/Home";
-import Navigation from "./routes/navigation/Navigation";
-import Authentication from "./routes/authentication/Authentication";
-import Shop from "./routes/shop/Shop";
-import Checkout from "./routes/checkout/Checkout";
 import { checkUserSession } from "./store/user/user.action";
+import Spinner from "./components/spinner/Spinner";
+
+// Lazy load the components to improve performance
+const Navigation = lazy(() => import("./routes/navigation/Navigation"));
+const Checkout = lazy(() => import("./routes/checkout/Checkout"));
+const Shop = lazy(() => import("./routes/shop/Shop"));
+const Home = lazy(() => import("./routes/home/Home"));
+const Authentication = lazy(() =>
+  import("./routes/authentication/Authentication")
+);
 
 const App = () => {
   const dispatch = useDispatch();
-
   // listen for changes in auth
   useEffect(() => {
     dispatch(checkUserSession());
   }, [dispatch]); // dispatch is not going to change
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
